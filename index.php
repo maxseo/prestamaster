e1707870cb5364a28bb910db0f55de9aec22938a
maxseo <maxseopro@gmail.com>
2023-01-21 15:33:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 2012 - 2020 Prestashop Master
+ *
+ * MODULE Prestashop Landing Page Builder
+ *
+ * @version   5.0.0
+ * @author    Prestashop Master <info@prestashopmaster.com>
+ * @link      https://www.prestashopmaster.com
+ * @copyright Copyright (c) permanent, Prestashop Master
+ * @license   Addons PrestaShop license limitation
+ *
+ * NOTICE OF LICENSE
+ *
+ * Don't use this module on several shops. The license provided by PrestaShop Addons
+ * for all its modules is valid only once for a single shop.
+ */
+
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+header("Location: ../");
+exit;
