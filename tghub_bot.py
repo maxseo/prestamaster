f85a728d821f68ef63a2f76102832f84af77a1e2
maxseo <maxseopro@gmail.com>
2023-02-16 02:01:56 +0300

    Update tghub_bot.py

--- tghub_bot.py
+++ tghub_bot.py
@@ -22,39 +22,37 @@ ws = wb.active
 ws.cell(row=1, column=1, value='Channel ID')
 ws.cell(row=1, column=2, value='Latest Posts')
 
-
 # Define function to collect latest posts for a channel
 async def collect_latest_posts(channel_id):
-    messages = await bot.get_history(chat_id=channel_id, limit=5)
+    try:
+        messages = await bot.get_history(chat_id=channel_id, limit=5)
 
-    # Write the posts to a CSV file
-    with open('posts.csv', 'a', encoding='utf-8', newline='') as file:
-        writer = csv.writer(file)
-        row = channel_ids.index(channel_id) + 2
-        for message in messages:
-            text = message.text
-            if message.photo:
-                photo_url = message.photo[-1].file_id
-                text = f'<a href="https://t.me/c/{channel_id}/{message.message_id}">&#8205;</a>\n<a href="{photo_url}">📷</a>\n{text}'
-            elif message.video:
-                video_url = message.video.file_id
-                text = f'<a href="https://t.me/c/{channel_id}/{message.message_id}">&#8205;</a>\n<a href="{video_url}">🎥</a>\n{text}'
-            else:
-                text = f'<a href="https://t.me/c/{channel_id}/{message.message_id}">{text}</a>'
+        # Write the posts to a CSV file
+        with open('posts.csv', 'a', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file)
+            row = channel_ids.index(channel_id) + 2
+            for message in messages:
+                text = message.text
+                if message.photo:
+                    photo_url = message.photo[-1].file_id
+                    text = f'<a href="https://t.me/c/{channel_id}/{message.message_id}">&#8205;</a>\n<a href="{photo_url}">📷</a>\n{text}'
+                elif message.video:
+                    video_url = message.video.file_id
+                    text = f'<a href="https://t.me/c/{channel_id}/{message.message_id}">&#8205;</a>\n<a href="{video_url}">🎥</a>\n{text}'
+                else:
+                    text = f'<a href="https://t.me/c/{channel_id}/{message.message_id}">{text}</a>'
 
-            writer.writerow([row, text])
-            row += 1
+                writer.writerow([row, text])
+                row += 1
 
-    except aiogram.exceptions.TelegramAPIError:
+    except aiogram.exceptions.TelegramAPIError as e:
         # Handle errors
-        print('Error collecting data for channel ID {}'.format(channel_id))
-
+        print(f'Error collecting data for channel ID {channel_id}: {e}')
 
 # Collect latest posts for each channel
 async def main():
     for channel_id in channel_ids:
         await collect_latest_posts(channel_id)
-
 
 # Run event loop and save workbook to file
 if __name__ == '__main__':
