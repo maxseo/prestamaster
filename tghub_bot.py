06784202485951cd7c0a3964bee81d13df0cca2b
maxseo <maxseopro@gmail.com>
2023-02-16 01:39:30 +0300

    Update tghub_bot.py

--- tghub_bot.py
+++ tghub_bot.py
@@ -1,43 +1,65 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+import asyncio
+import aiogram
+import openpyxl
 
-import os
+# Load bot token from file
+with open('bot_token.txt', 'r') as f:
+    bot_token = f.read().strip()
 
-bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+# Create bot and event loop
+bot = aiogram.Bot(token=bot_token)
+loop = asyncio.get_event_loop()
 
-async def on_startup(_):
-    print('Я в онлайне')
+# Load channel IDs from file
+with open('channels.txt', 'r') as f:
+    channel_ids = [line.strip() for line in f]
 
-'''****Client***'''
-@dp.message_handler(commands=['start','help'])
-async def command_start(message : types.Message):
-    try:
-        await bot.send_message(message.from_user.id, 'Ништяк')
-        await message.delete()
-    except:
-        await message.reply('Общение с ботом через ЛС\nссылка')
+# Create workbook and worksheet
+wb = openpyxl.Workbook()
+ws = wb.active
 
-@dp.message_handler(commands=['Режим работы'])
-async  def bot_open_command(message : types.Message):
-    await bot.send_message(message.from_user.id, '24/7')
-
-@dp.message_handler(commands=['Реклама'])
-async  def bot_adv_command(message : types.Message):
-    await bot.send_message(message.from_user.id, 'Напишите админу')
-
-@dp.message_handler(commands=['Меню'])
-async  def bot_menu_command(message : types.Message):
-    for ret in cur.execute('SELECT * FROM menu').fetchall():
-        await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена: {ret[-1]}')
-
-@dp.message_handler()
-async def echo_send(message : types.Message):
-    if message.text == 'Прив':
-        await message.answer('На' +message.text + 'обед')
-    # await message.reply(message.text)
-    # await bot.send_message(message.from_user.id, message.text)
+# Write header row to worksheet
+ws.cell(row=1, column=1, value='Channel ID')
+ws.cell(row=1, column=2, value='Latest Posts')
 
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+# Define function to collect latest posts for a channel
+    async def collect_latest_posts(channel_id):
+        # Get the latest 5 messages from the channel
+        messages = await bot.get_messages(chat_id=channel_id, limit=5)
+
+        # Format the messages as HTML
+        posts = []
+        for message in messages:
+            if message.photo:
+                post_content = f'<a href="{message.link}">&#8205;</a><a href="{message.photo[-1].file_id}">&#8205;</a>'
+            elif message.video:
+                post_content = f'<a href="{message.link}">&#8205;</a><a href="{message.video.file_id}">&#8205;</a>'
+            else:
+                post_content = message.text_html
+            posts.append(f'<div class="tg-post">{post_content}</div>')
+
+        # Return the formatted posts
+        return posts
+
+                # Write channel ID and latest post to worksheet
+                row = channel_ids.index(channel_id) + 2
+                ws.cell(row=row, column=1, value=channel_id)
+                ws.cell(row=row, column=2, value=post_html)
+                break
+
+    except aiogram.exceptions.TelegramAPIError:
+        # Handle errors
+        print('Error collecting data for channel ID {}'.format(channel_id))
+
+
+# Collect latest posts for each channel
+async def main():
+    for channel_id in channel_ids:
+        await collect_latest_posts(channel_id)
+
+
+# Run event loop and save workbook to file
+if __name__ == '__main__':
+    loop.run_until_complete(main())
+    wb.save('latest_posts.xlsx')
